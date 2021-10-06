5f8a036e0238b8350010e544ef9ddc8089ec6fc8
Seredyukb <seredyuk.b@gmail.com>
2021-10-06 23:59:00 +0300

    add new Componentas

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = ()=>{
+return(
+  <div>
+Ya FOOTER!
+
+  </div>
+)
+
+}
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = ()=> {
+return(
+  <div>
+    Ya Header!
+
+  </div>
+)
+
+}
+export default Header
