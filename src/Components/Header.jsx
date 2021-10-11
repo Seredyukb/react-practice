ac52965dfd017cdec8dcd09736840596f402d209
Seredyukb <seredyuk.b@gmail.com>
2021-10-11 21:32:31 +0300

    add css modules

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from './Header.module.css';
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img src="https://i.pinimg.com/originals/3f/3d/d9/3f3dd9219f7bb1c9617cf4f154b70383.jpg" />
         </header>
 
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import classes from './Profile.module.css';
+
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <img src='https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' />
             <div>
                 ava + description
             </div>
             <div>
                 My posts
-                <div>Post 1</div>
-                <div>Post 2</div>
+                <div className={classes.item}>Post 1</div>
+                <div className={classes.item}>Post 2</div>
             </div>
         </div>
     )
