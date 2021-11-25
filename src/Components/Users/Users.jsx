02146538082de4bdec09090435246c6000353b2e
Seredyukb <seredyuk.b@gmail.com>
2021-11-26 00:22:50 +0300

    add new func to class Users componentDidMount

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,12 +4,9 @@ import classes from './Users.module.css';
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component{
-    constructor(props){
-        super(props);
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response=>{
-                this.props.setUsers(response.data.items);
-            })
-    }
+    componentDidMount(){  axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response=>{
+        this.props.setUsers(response.data.items);
+    })};
     
 render(){
     return <div>
