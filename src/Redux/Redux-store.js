9f71fe1c9fa67a9ed8f23707812e6b6f35535dbc
Seredyukb <seredyuk.b@gmail.com>
2021-12-23 01:58:49 +0300

    add chrome extension ReduxDevTools

--- src/Redux/Redux-store.js
+++ src/Redux/Redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import dialogsReducer from "./DialogsReducer";
 import profileReducer from "./ProfileReducer";
 import friendsReducer from "./FriendReducer";
@@ -20,8 +20,15 @@ let reducers = combineReducers(
         form: formReducer,
         app: appReducer,
     })
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
-window.store = store;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)
+));
+
+
+
+
+window.__store__ = store;
 
 export default store;
